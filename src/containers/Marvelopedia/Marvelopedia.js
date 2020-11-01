04bcffa5e28d0e898f1eb70522b36f024baba3db
karosowka13 <sowa@student.agh.edu.pl>
2020-11-01 14:00:45 +0100

    combine reducers, add redux-thunk

--- src/containers/Marvelopedia/Marvelopedia.js
+++ src/containers/Marvelopedia/Marvelopedia.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, withRouter, Switch } from "react-router-dom";
+import * as actions from "../../store/actions/index";
 
 import Modal from "../../components/UI/Modal/Modal";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import classes from "Marvelopedia.module.css";
 
 class Marvelopedia extends Component {
+	componentDidMount() {
+		this.props.fetchProducts();
+	}
 	render() {
 		return (
 			<React.Fragment>
@@ -26,4 +32,26 @@ class Marvelopedia extends Component {
 	}
 }
 
-export default Marvelopedia;
+const mapStateToProps = (state) => {
+	return {
+		loading: state.loadTraininglog.loading,
+		error: state.loadTraininglog.error,
+		userId: state.auth.userId,
+		month: state.date.month,
+		trainings: state.loadTraininglog.trainings,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		traininglogData: (trainingLog, userId) =>
+			dispatch(actions.loadTraininglog(trainingLog, userId)),
+		nextMonth: (month) => dispatch(actions.nextMonth(month)),
+		prevMonth: (month) => dispatch(actions.prevMonth(month)),
+		onDayClick: (day) => dispatch(actions.onDateClick(day)),
+	};
+};
+
+export default withRouter(
+	connect(mapStateToprops, mapDispatchToprops)(Marvelopedia)
+);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+import authReducer from "./store/reducers/auth";
+import charactersReducer from "./store/reducers/characters";
+
+const composeEnhancers =
+	process.env.NODE_ENV === "development"
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
+
+const rootReducer = combineReducers({
+	characters: charactersReducer,
+	auth: authReducer,
+});
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
